dfb150b12c27a16b98046f2c9c6df0eb932bb620
Celer <celer8270@gmail.com>
2026-04-09 20:23:43 +0100

    Don't bother with author

--- src/paths.py
+++ src/paths.py
@@ -2,15 +2,14 @@ from pathlib import Path
 from appdirs import user_data_dir, user_config_dir
 
 APP_NAME = "super_sudoku_solver"
-APP_AUTHOR = "matilda"  # Only used by Windows
 
 SRC_DIR = Path(__file__).resolve().parent
-PUZZLE_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR))
+PUZZLE_DIR = Path(user_data_dir(APP_NAME))
 PUZZLE_JSON = PUZZLE_DIR / "puzzles.json"
 PUZZLE_DATA = PUZZLE_DIR / ".data"
 GUESSES_SUFFIX = "_guesses.npy"
 CANDIDATES_SUFFIX = "_candidates.npy"
-CONFIG_DIR = Path(user_config_dir(APP_NAME, APP_AUTHOR))
+CONFIG_DIR = Path(user_config_dir(APP_NAME))
 SETTINGS = CONFIG_DIR / "settings.toml"
 
 if not PUZZLE_DATA.exists():
