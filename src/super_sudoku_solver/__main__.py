396f964e9ef859c9ef7ee399d41379077ac02807
Celer <celer8270@gmail.com>
2026-04-14 00:43:59 +0100

    Bug fix. Stop puzzles being added twice.

--- src/super_sudoku_solver/__main__.py
+++ src/super_sudoku_solver/__main__.py
@@ -1,14 +1,10 @@
 """
-Main entry point for program.
+File to allow module to be ran with `python -m super_sudoku_solver` as 
+alternative to gui-script generated after installation.
 """
 
 import super_sudoku_solver.entry_points as entry_points
 
-
-def main():
-    entry_points.main()
-
-
-# __main__.py files typically don't check __name__
+# __main__.py files typically don't check __name__ as they can't be imported
 # https://docs.python.org/3/library/__main__.html#main-py-in-python-packages
-main()
+entry_points.main()
