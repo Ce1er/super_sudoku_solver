840467a51e5f3a62c7852785e4c1978a932bb357
Celer <celer8270@gmail.com>
2026-04-16 01:19:51 +0100

    type keyword for type alias

--- src/super_sudoku_solver/custom_types.py
+++ src/super_sudoku_solver/custom_types.py
@@ -1,21 +1,21 @@
 import numpy as np
 from typing import Literal
 
-Adjacency = Literal["row", "column", "box"]
+type Adjacency = Literal["row", "column", "box"]
 
 # Common array types with shape and dtype.
 
 # Values: [row, column]
-Coord = np.ndarray[tuple[Literal[2]], np.dtype[np.int8]]
+type Coord = np.ndarray[tuple[Literal[2]], np.dtype[np.int8]]
 
-Coords = np.ndarray[tuple[int, Literal[2]], np.dtype[np.int8]]
+type Coords = np.ndarray[tuple[int, Literal[2]], np.dtype[np.int8]]
 
 # Values: [row, column, value]
-Cell = np.ndarray[tuple[Literal[3]], np.dtype[np.int8]]
+type Cell = np.ndarray[tuple[Literal[3]], np.dtype[np.int8]]
 
 # Indexes: [row, column]
-Cells = np.ndarray[tuple[Literal[9], Literal[9]], np.dtype[np.int8]]
-CellCandidates = np.ndarray[tuple[Literal[9]], np.dtype[np.bool]]
+type Cells = np.ndarray[tuple[Literal[9], Literal[9]], np.dtype[np.int8]]
+type CellCandidates = np.ndarray[tuple[Literal[9]], np.dtype[np.bool]]
 
 # Indexes: [value, row, column]
-Candidates = np.ndarray[tuple[Literal[9], Literal[9], Literal[9]], np.dtype[np.bool]]
+type Candidates = np.ndarray[tuple[Literal[9], Literal[9], Literal[9]], np.dtype[np.bool]]
