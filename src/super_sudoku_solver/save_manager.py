4c618d2d89c02ecbc6c490a912a61d6a1bfd5f34
Celer <celer8270@gmail.com>
2026-04-18 22:24:57 +0100

    Refactor

--- src/super_sudoku_solver/save_manager.py
+++ src/super_sudoku_solver/save_manager.py
@@ -204,12 +204,11 @@ class Puzzle:
 
     @property
     def clues(self) -> Cells:
-        if self._clues is not None:
-            return self._clues.copy()
+        if self._clues is None:
+            values = [-1 if clue == "." else int(clue) - 1 for clue in self._str_clues]
+            self._clues: Cells = np.array(values, dtype=np.int8).reshape((9, 9))
+            self._clues.flags.writeable = False
 
-        values = [-1 if clue == "." else int(clue) - 1 for clue in self._str_clues]
-        self._clues: Cells = np.array(values, dtype=np.int8).reshape((9, 9))
-        self._clues.flags.writeable = False
         return self._clues.copy()
 
     @property
